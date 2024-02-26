6fe35d9c89a04919a0c419327b3bb0517df3007e
Philippe Dube <philippedube@outlook.com>
2024-02-26 12:10:30 -0500

    Fixed some i18n issues

--- i18n/en.php
+++ i18n/en.php
@@ -1,6 +1,11 @@
 <?php
 //Language translation for English Keys
 $lang = array(
+    "lang.code" => "en",
+    "selector.french" => "Français",
+    "selector.english" => "English",
+    "page-not-found" => "Sorry the page was not found!",
+    "title" => "Website title",
     "welcome" => "Welcome to our website!",
 );
 ?>
--- i18n/fr.php
+++ i18n/fr.php
@@ -1,6 +1,11 @@
 <?php
 //Language translation for French Keys
 $lang = array(
-    "welcome" => "Bienvenue sur notre site web!",
+    "lang.code" => "fr",
+    "selector.french" => "Français",
+    "selector.english" => "English",
+    "page-not-found" => "Désolé la page n'as pas pus être trouvé !",
+    "title" => "Titre du site",
+    "welcome" => "Bienvenu sur notre site !",
 );
 ?>
