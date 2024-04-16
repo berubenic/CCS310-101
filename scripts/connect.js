e17d99c7527619785610dd5a852be1529459ea7f
Philippe Dube <philippedube@outlook.com>
2024-04-16 19:18:16 -0400

    Fixed v3 validation

--- scripts/connect.js
+++ scripts/connect.js
@@ -5,10 +5,24 @@ window.addEventListener('load', function() {
     // Loop over them and prevent submission
     Array.prototype.filter.call(forms, function(form) {
         form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-            } else {
+            event.preventDefault();
+            event.stopPropagation();
+
+            // Check each input field in the form
+            Array.prototype.filter.call(form.elements, function(element) {
+                if (element.type !== 'button') {
+                    var feedback = element.parentNode.querySelector('.invalid-feedback-custom');
+                    if (element.checkValidity() === false) {
+                        // If the input is invalid, add your custom invalid feedback class
+                        feedback.style.display = 'block';
+                    } else {
+                        // If the input is valid, remove your custom invalid feedback class
+                        feedback.style.display = 'none';
+                    }
+                }
+            });
+
+            if (form.checkValidity() === true) {
                 // Display the loading div
                 document.querySelector('#loadingBlocker').style.display = 'block';
 
@@ -22,16 +36,17 @@ window.addEventListener('load', function() {
                     successDiv.style.display = 'block';
 
                     // Clear all the input fields in the form
-                    form.reset();
+                    Array.prototype.filter.call(form.elements, function(element) {
+                        if (element.type !== 'button') {
+                            element.value = '';
+                        }
+                    });
 
                     // Hide the success message after 5 seconds
                     setTimeout(function () {
                         successDiv.style.display = 'none';
                     }, 5000);
                 }, 5000);
-
-                event.preventDefault();
-                event.stopPropagation();
             }
 
             form.classList.add('was-validated');
